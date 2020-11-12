495a10ba7be86c4568f493d4972a53211100f820
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-11-12 13:57:24 -0800

    admin delete products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,10 +5,11 @@ import Product from "../models/productModel.js";
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts);
-
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
