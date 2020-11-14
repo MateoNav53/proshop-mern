676fe60a4c7b7d9399d8c05938400721b3468480
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-11-13 17:27:28 -0800

    admin order list

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -109,7 +109,7 @@ const ProfileScreen = ({ location, history }) => {
         ) : errorOrders ? (
           <Message variant="danger">{errorOrders}</Message>
         ) : (
-          <Table striped bordered hover responsive className="table-sm">
+          <Table striped border hover responsive className="table-sm">
             <thead>
               <tr>
                 <th>ID</th>
